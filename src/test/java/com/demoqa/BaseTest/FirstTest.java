d7e2f5369c917b004cccfc007a5b241707810842
Davtyan Samvel <dvatyan11@gmail.com>
2025-02-12 15:59:42 +0300

    create project for demoQA

---
+++ src/test/java/com/demoqa/BaseTest/FirstTest.java
@@ -0,0 +1,18 @@
+package com.demoqa.BaseTest;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class FirstTest {
+    @Test
+    public  void OpenDemoQASite(){
+        Configuration.browser = "chrome"; // Указываем браузер
+        open("https://demoqa.com");      // Открываем сайт
+        $(".banner-image").shouldBe(visible); // Проверяем, что элемент виден
+    }
+}
